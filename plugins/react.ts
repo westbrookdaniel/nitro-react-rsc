a87a67b6a46aeffd69681e6705705e2c6c2c0728
westy12dan <westy12dan@gmail.com>
2023-11-07 08:37:40 +0800

    wip

--- plugins/react.ts
+++ plugins/react.ts
@@ -1,10 +1,22 @@
 import { isValidElement } from "react";
 import { renderToPipeableStream } from "react-dom/server";
+import { createServer as createViteDevServer } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook("beforeResponse", (_event, response) => {
-    if (isValidElement(response.body)) {
-      response.body = renderToPipeableStream(response.body);
+export default defineNitroPlugin(async (nitroApp) => {
+  const viteDevServer = await createViteDevServer({
+    plugins: [react()],
+    server: { middlewareMode: true },
+  });
+
+  nitroApp.hooks.hook("beforeResponse", (event, response) => {
+    if (event.headers.get("accept").includes("text/x-component")) {
+      // TODO?
+    } else {
+      if (isValidElement(response.body)) {
+        response.body = renderToPipeableStream(response.body);
+        event.node.req.headers["content-type"] = "text/html";
+      }
     }
   });
 });
